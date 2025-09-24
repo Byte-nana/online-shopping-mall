a56687a60dc5fa35183609d98f20c8c010f43213
Byte-nana <nanadevelop44@gmail.com>
2025-09-24 08:04:59 +0100

    init: first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+import Nav from './components/Nav/Nav.jsx';
+
+function App() {
+  return (
+    <>
+      <header>
+        <Nav />
+      </header>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Nav.module.css';
+
+export default function Nav() {
+  return (
+    <div>
+      <h1>VeggiePop</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Menu</li>
+          <li>About Us</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <div></div>
+    </div>
+  );
+}
